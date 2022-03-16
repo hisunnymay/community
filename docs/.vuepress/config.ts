6ba78f7a6b14d46416e886c15420f573cbe5bca8
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2022-03-15 23:26:36 -0700

    more change

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -54,13 +54,13 @@ export default defineUserConfig<DefaultThemeOptions>({
   locales: {
     '/': {
       lang: 'en-US',
-      title: 'VuePress',
-      description: 'Vue-powered Static Site Generator',
+      title: 'Framely',
+      description: 'Conversational Service Made Easy',
     },
     '/zh/': {
       lang: 'zh-CN',
-      title: 'VuePress',
-      description: 'Vue 驱动的静态网站生成器',
+      title: 'Framely',
+      description: '让天下没有难的对话式服务',
     },
   },
 
