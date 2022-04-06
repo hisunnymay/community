4d8b71b629001573edb89e9a21088c1d72be7e76
karani <karanijohn018@gmail.com>
2022-04-06 13:55:29 +0300

    initial blog

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -60,7 +60,7 @@ export default defineUserConfig<DefaultThemeOptions>({
     '/': {
       lang: 'en-US',
       title: 'Framely',
-      description: 'Composable Conversational Services Made Easy',
+      description: 'Composable Conversational Interface for Services',
     },
     // '/zh/': {
     //  lang: 'zh-CN',
@@ -97,6 +97,8 @@ export default defineUserConfig<DefaultThemeOptions>({
 
         // page meta
         editLinkText: 'Edit this page on GitHub',
+
+        darkMode: true,
       },
 
       /**
@@ -149,6 +151,7 @@ export default defineUserConfig<DefaultThemeOptions>({
 
       // use shiki plugin in production mode instead
       prismjs: !isProd,
+
     },
   },
 
@@ -167,66 +170,66 @@ export default defineUserConfig<DefaultThemeOptions>({
   },
 
   plugins: [
-    [
-      '@vuepress/plugin-docsearch',
-      {
-        apiKey: '3a539aab83105f01761a137c61004d85',
-        indexName: 'vuepress',
-        searchParameters: {
-          facetFilters: ['tags:v2'],
-        },
-        locales: {
-          '/zh/': {
-            placeholder: '搜索文档',
-            translations: {
-              button: {
-                buttonText: '搜索文档',
-                buttonAriaLabel: '搜索文档',
-              },
-              modal: {
-                searchBox: {
-                  resetButtonTitle: '清除查询条件',
-                  resetButtonAriaLabel: '清除查询条件',
-                  cancelButtonText: '取消',
-                  cancelButtonAriaLabel: '取消',
-                },
-                startScreen: {
-                  recentSearchesTitle: '搜索历史',
-                  noRecentSearchesText: '没有搜索历史',
-                  saveRecentSearchButtonTitle: '保存至搜索历史',
-                  removeRecentSearchButtonTitle: '从搜索历史中移除',
-                  favoriteSearchesTitle: '收藏',
-                  removeFavoriteSearchButtonTitle: '从收藏中移除',
-                },
-                errorScreen: {
-                  titleText: '无法获取结果',
-                  helpText: '你可能需要检查你的网络连接',
-                },
-                footer: {
-                  selectText: '选择',
-                  navigateText: '切换',
-                  closeText: '关闭',
-                  searchByText: '搜索提供者',
-                },
-                noResultsScreen: {
-                  noResultsText: '无法找到相关结果',
-                  suggestedQueryText: '你可以尝试查询',
-                  openIssueText: '你认为该查询应该有结果？',
-                  openIssueLinkText: '点击反馈',
-                },
-              },
-            },
-          },
-        },
-      },
-    ],
-    [
-      '@vuepress/plugin-google-analytics',
-      {
-        // we have multiple deployments, which would use different id
-        id: process.env.DOCS_GA_ID,
-      },
-    ],
+  //   [
+  //     '@vuepress/plugin-docsearch',
+  //     {
+  //       apiKey: '3a539aab83105f01761a137c61004d85',
+  //       indexName: 'vuepress',
+  //       searchParameters: {
+  //         facetFilters: ['tags:v2'],
+  //       },
+  //       locales: {
+  //         '/zh/': {
+  //           placeholder: '搜索文档',
+  //           translations: {
+  //             button: {
+  //               buttonText: '搜索文档',
+  //               buttonAriaLabel: '搜索文档',
+  //             },
+  //             modal: {
+  //               searchBox: {
+  //                 resetButtonTitle: '清除查询条件',
+  //                 resetButtonAriaLabel: '清除查询条件',
+  //                 cancelButtonText: '取消',
+  //                 cancelButtonAriaLabel: '取消',
+  //               },
+  //               startScreen: {
+  //                 recentSearchesTitle: '搜索历史',
+  //                 noRecentSearchesText: '没有搜索历史',
+  //                 saveRecentSearchButtonTitle: '保存至搜索历史',
+  //                 removeRecentSearchButtonTitle: '从搜索历史中移除',
+  //                 favoriteSearchesTitle: '收藏',
+  //                 removeFavoriteSearchButtonTitle: '从收藏中移除',
+  //               },
+  //               errorScreen: {
+  //                 titleText: '无法获取结果',
+  //                 helpText: '你可能需要检查你的网络连接',
+  //               },
+  //               footer: {
+  //                 selectText: '选择',
+  //                 navigateText: '切换',
+  //                 closeText: '关闭',
+  //                 searchByText: '搜索提供者',
+  //               },
+  //               noResultsScreen: {
+  //                 noResultsText: '无法找到相关结果',
+  //                 suggestedQueryText: '你可以尝试查询',
+  //                 openIssueText: '你认为该查询应该有结果？',
+  //                 openIssueLinkText: '点击反馈',
+  //               },
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //   ],
+  //   [
+  //     '@vuepress/plugin-google-analytics',
+  //     {
+  //       // we have multiple deployments, which would use different id
+  //       id: process.env.DOCS_GA_ID,
+  //     },
+  //   ],
     [
       '@vuepress/plugin-register-components',
       {
@@ -282,14 +285,14 @@ export default defineUserConfig<DefaultThemeOptions>({
       }
     ],
 
-    // only enable shiki plugin in production mode
-    [
-      '@vuepress/plugin-shiki',
-      isProd
-        ? {
-            theme: 'dark-plus',
-          }
-        : false,
-    ],
+  //   // only enable shiki plugin in production mode
+  //   [
+  //     '@vuepress/plugin-shiki',
+  //     isProd
+  //       ? {
+  //           theme: 'dark-plus',
+  //         }
+  //       : false,
+  //   ],
   ],
 })
---
+++ docs/posts/new-blog-article.md
@@ -0,0 +1,11 @@
+---
+date: 2022-01-03
+category:
+  - CategoryA
+  - CategoryB
+tag:
+  - tag A
+  - tag B
+---
+
+# New twitter artcle to checkout
