54f7a523e6722f09ad9cc02f3b732683707cb3e4
karani <karanijohn018@gmail.com>
2022-04-20 09:30:42 +0300

    carousel

--- docs/.vuepress/components/home/HomePage.vue
+++ docs/.vuepress/components/home/HomePage.vue
@@ -5,11 +5,12 @@ import HomeFooter from '@theme/HomeFooter.vue'
 import HomeHero from '@theme/HomeHero.vue'
 import CTA from '../cta/callToAction.vue'
 import ContentCard from '../contentCard/ContentCard.vue'
+import Hero from "../hero/hero.vue"
 </script>
 
 <template>
   <main class="home">
-    <HomeHero />
+    <Hero />
     <HomeFeatures />
     <HomeContent />
     <ContentCard />
